c1bd05debe3402b94a824e8d33016ce844a4f387
Joseph Holland <jh2044@cam.ac.uk>
2020-04-09 12:22:27 +0100

    started improving trough finding robustness for dropped stitches

--- mesh_profile.m
+++ mesh_profile.m
@@ -1,4 +1,5 @@
-function profile_mesh = mesh_profile(sample,calib, x_space, y_space, x_initial, y_initial)
+function profile_mesh = mesh_profile(sample, calib, x_space, y_space, x_initial, y_initial)
+
 % correct raw data for laser head rotation
 theta = deg2rad(0.507);
 
@@ -7,6 +8,7 @@ sample(:,3) = sample(:,3)*cos(theta) - sample(:,1)*sin(theta);
 
 calib(:,1) = calib(:,1)*cos(theta) + calib(:,3)*sin(theta);
 calib(:,3) = calib(:,3)*cos(theta) - calib(:,1)*sin(theta);
+
 
 % generate uniform mesh space
 [profile_mesh.x_mesh,profile_mesh.y_mesh] = meshgrid(min(sample(:,1)+sample(:,4)+x_initial):x_space:max(sample(:,1)+sample(:,4)+x_initial),...
@@ -17,6 +19,7 @@ profile_mesh.z_mesh = griddata(sample(:,1)+sample(:,4)+x_initial,sample(:,2)+y_i
 % and linear interpolation on the calibration surface
 profile_mesh.z_calib = griddata(calib(:,1)+calib(:,4),calib(:,2),calib(:,3),...
                                 profile_mesh.x_mesh,profile_mesh.y_mesh);
-                            
+
+% subtract empty bed calibration from scan surface                            
 profile_mesh.z_mesh = profile_mesh.z_mesh - profile_mesh.z_calib;
 end
